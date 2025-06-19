b95df7538790d7fe89de6735b5840fac3c9b9f4d
rohaidi869 <rohaidifamily@gmail.com>
2025-06-19 13:49:00 +0800

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,28 @@
+const articles = [
+  {
+    id: "tips-belajar-arab",
+    title: "Tips Belajar Bahasa Arab",
+    date: "2025-06-19",
+    category: "Pendidikan",
+    tags: ["Bahasa", "Pembelajaran"],
+    content: `
+      <p>Bahasa Arab adalah bahasa yang kaya dan menarik. Berikut beberapa tips belajar Bahasa Arab dengan lebih berkesan:</p>
+      <ul>
+        <li>Berlatih setiap hari</li>
+        <li>Mendengar dan menonton bahan dalam Bahasa Arab</li>
+        <li>Bergabung dengan komuniti pembelajar</li>
+      </ul>
+    `
+  },
+  {
+    id: "cara-bina-blog",
+    title: "Cara Membina Blog Mudah",
+    date: "2025-06-18",
+    category: "Teknologi",
+    tags: ["Blog", "Teknologi"],
+    content: `
+      <p>Ini adalah panduan asas membina blog menggunakan HTML, CSS, dan JavaScript sahaja.</p>
+    `
+  }
+  // Tambah artikel baru di sini!
+];
