a29a57e0261523c0ad3318ef3e3bc59102910177
rohaidi869 <rohaidifamily@gmail.com>
2025-06-19 20:09:49 +0800

    Update articles.js

--- articles.js
+++ articles.js
@@ -7,80 +7,51 @@ const articles = [
     tags: ["Bahasa", "Pembelajaran"],
     content: `
       <p>Bahasa Arab Untuk Tujuan Khusus, atau Arabic for Specific Purposes (ASP), ialah satu cabang pembelajaran bahasa Arab yang dirancang khusus untuk memenuhi keperluan dan objektif tertentu yang bersifat spesifik, berbeza daripada pembelajaran bahasa Arab secara umum yang lebih menekankan penguasaan asas tatabahasa dan kemahiran komunikasi harian[1][2][3]. Dalam konteks ini, pembelajaran bahasa Arab disesuaikan dengan latar belakang, minat, serta keperluan pembelajar seperti bidang akademik, profesional, perniagaan, keagamaan, diplomasi, teknologi, dan sebagainya[1][4].</p>
-<p>&nbsp;Perbezaan Dengan Pembelajaran Bahasa Arab Umum</p>
+<p><strong>Perbezaan Dengan Pembelajaran Bahasa Arab Umum</strong></p>
 <p>- Bahasa Arab Umum bertujuan untuk menguasai kemahiran asas seperti membaca, menulis, berbicara, dan memahami bahasa Arab dalam kehidupan seharian.</p>
 <p>- Bahasa Arab Untuk Tujuan Khusus pula menumpukan kepada penguasaan bahasa Arab dalam konteks tertentu, misalnya untuk memahami teks akademik, berkomunikasi dalam bidang perubatan, perbankan, pelancongan, atau keagamaan[1][2][3][4].</p>
-<table>
+<table width="619">
 <tbody>
 <tr>
-<td width="200">
-<p>Aspek</p>
-</td>
-<td width="200">
-<p>Bahasa Arab Umum</p>
-</td>
-<td width="200">
-<p>Bahasa Arab Untuk Tujuan Khusus</p>
-</td>
+<td>Aspek</td>
+<td>Bahasa Arab Umum</td>
+<td>Bahasa Arab Untuk Tujuan Khusus</td>
 </tr>
 <tr>
-<td width="200">
-<p>Skop</p>
-</td>
-<td width="200">
-<p>Luas, umum, harian</p>
-</td>
-<td width="200">
-<p>Spesifik, mengikut bidang/minat</p>
-</td>
+<td>Skop</td>
+<td>Luas, umum, harian</td>
+<td>Spesifik, mengikut bidang/minat</td>
 </tr>
 <tr>
-<td width="200">
-<p>Tujuan</p>
-</td>
-<td width="200">
-<p>Penguasaan asas bahasa</p>
-</td>
-<td width="200">
-<p>Penguasaan bidang tertentu</p>
-</td>
+<td>Tujuan</td>
+<td>Penguasaan asas bahasa</td>
+<td>Penguasaan bidang tertentu</td>
 </tr>
 <tr>
-<td width="200">
-<p>Kurikulum</p>
-</td>
-<td width="200">
-<p>Umum, standard</p>
-</td>
-<td width="200">
-<p>Disusun khusus, fleksibel</p>
-</td>
+<td>Kurikulum</td>
+<td>Umum, standard</td>
+<td>Disusun khusus, fleksibel</td>
 </tr>
 <tr>
-<td width="200">
-<p>Contoh</p>
-</td>
-<td width="200">
-<p>Komunikasi seharian</p>
-</td>
-<td width="200">
-<p>Bahasa Arab untuk perbankan, agama</p>
-</td>
+<td>Contoh</td>
+<td>Komunikasi seharian</td>
+<td>Bahasa Arab untuk perbankan, agama</td>
 </tr>
 </tbody>
 </table>
-<p>Ciri-ciri Bahasa Arab Untuk Tujuan Khusus</p>
+<p><strong>Ciri-ciri Bahasa Arab Untuk Tujuan Khusus</strong></p>
 <p>- Kurikulum dan bahan pembelajaran disusun secara khusus dan sistematik berdasarkan keperluan serta latar belakang pembelajar[2][3].</p>
 <p>- Tidak terikat kepada satu kaedah tertentu; pendekatan pengajaran lebih fleksibel dan menitikberatkan minat serta motivasi pelajar[2].</p>
 <p>- Penekanan kepada penguasaan kosa kata, istilah, dan frasa yang relevan dengan bidang yang dipelajari, contohnya istilah perubatan, istilah agama, atau istilah perniagaan[4].</p>
 <p>- Bertujuan untuk meningkatkan kemahiran komunikasi dalam konteks tertentu, sama ada secara lisan atau tulisan, serta membina kompetensi gramatikal, sosiolinguistik, analisis wacana, dan kompetensi strategik[2].</p>
-<p>Kepentingan dan Aplikasi</p>
+<p><strong>Kepentingan dan Aplikasi</strong></p>
 <p>Bahasa Arab Untuk Tujuan Khusus sangat penting dalam dunia moden kerana keperluan komunikasi dan akses maklumat dalam pelbagai bidang semakin meningkat. Contoh aplikasi termasuk:</p>
 <p>- Akademik: Memahami buku rujukan, jurnal, dan bahan ilmiah dalam bahasa Arab.</p>
 <p>- Profesi: Berkomunikasi dengan pelanggan atau rakan sekerja dalam bidang perbankan, pelancongan, diplomasi, perubatan, dan lain-lain.</p>
 <p>- Keagamaan: Mendalami ilmu-ilmu Islam seperti Syariah, Usuluddin, dan pengajian al-Quran dengan lebih tepat kerana sumber utama ilmu tersebut adalah dalam bahasa Arab[4][5].</p>
-<p>Kesimpulan</p>
+<p><strong>Kesimpulan</strong></p>
 <p>Bahasa Arab Untuk Tujuan Khusus merupakan satu pendekatan pembelajaran bahasa yang sangat relevan dan praktikal pada masa kini. Ia bukan sahaja membantu pelajar mencapai objektif pembelajaran yang lebih fokus, malah meningkatkan daya saing dan profesionalisme dalam pelbagai bidang. Dengan kurikulum yang fleksibel dan berorientasikan keperluan sebenar, pembelajaran bahasa Arab kini lebih dinamik dan mampu memenuhi tuntutan masyarakat global yang semakin kompleks[1][2][3][4].</p>
+<p><strong>Rujukan </strong></p>
 <p>[1] https://www.muhammadthohir.com/2019/02/bahasa-arab-tujuan-khusus.html</p>
 <p>[2] https://ejournal.unhasy.ac.id/index.php/alfusha/article/download/1220/902</p>
 <p>[3] https://idr.uin-antasari.ac.id/17803/1/BAHASA%20ARAB%20UNTUK%20TUJUAN%20KHUSUS%20(ONLINE).pdf</p>
