80471a08563bd49f971fe7167e3e48aee22dabc1
rohaidi869 <rohaidifamily@gmail.com>
2025-06-23 11:27:11 +0800

    Update articles.js

--- articles.js
+++ articles.js
@@ -123,7 +123,7 @@ const articles = [
 <p>Mat Daud, N., &amp; Abu Kassim, N. L. (2011). Pengajaran Bahasa Arab untuk tujuan khusus di Malaysia: Satu tinjauan. *GEMA Online Journal of Language Studies, 11*(1), 129-142.</p>
 <p>Saleh, M. (2013). Arabic for Specific Purposes (ASP): A new trend in teaching and learning Arabic language. *International Journal of Social Science and Humanity, 3*(5), 500&ndash;503. https://doi.org/10.7763/IJSSH.2013.V3.293</p>    `
 
-  }
+  },
   {
     id: "Perbezaan ASP dan Umum",
     title: "Perbezaan Antara Bahasa Arab Untuk Tujuan Khusus dan Bahasa Arab Umum",
@@ -227,5 +227,5 @@ const articles = [
 <p>Saleh, M. (2013). Arabic for Specific Purposes (ASP): A new trend in teaching and learning Arabic language. <em>International Journal of Social Science and Humanity, 3</em>(5), 500&ndash;503. <a href="https://doi.org/10.7763/IJSSH.2013.V3.293">https://doi.org/10.7763/IJSSH.2013.V3.293</a></p>
 <p>Tengkari, A. (2007). The difference between Arabic for specific purpose and Arabic for general purpose. <em>Issues in Language Studies, 8</em>(1), 1&ndash;15.</p>
 <p>Nadwah, A. (1998). Arabic for specific purposes: A literature review. <em>Issues in Language Studies, 8</em>(1), 1&ndash;15.</p>
-},
+}
 ];
